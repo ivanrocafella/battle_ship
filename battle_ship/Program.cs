7f483bdb312eea7bffadf7ebeaaa8d92fc758f0e
Ivan Kobtsev <vanomc77@gmail.com>
2022-07-01 12:57:54 +0600

    update MakeMap

--- battle_ship/Program.cs
+++ battle_ship/Program.cs
@@ -6,41 +6,45 @@ namespace battle_ship
 {
     class Program
     {
-        public static Dictionary<int, string> ships;
+        public static Dictionary<int, string> shipsPlayer;
+        public static Dictionary<int, string> shipsComputer;
         static void Main(string[] args)
         {
-            ships = MakeNewDictWithSpace();
+            shipsPlayer = MakeNewDictWithSpace();
+            shipsComputer = MakeNewDictWithSpace();
             // ships[55] = "o";
             // ships = MakeSectionOfShip(ships, "4", 5, 6);
             // ships = MakeShot(ships, 7, 6);
-            PutNewShip(ships);
-            Console.WriteLine(MakeMap(ships));
+            PutNewShip(shipsComputer);
+            Console.WriteLine(MakeMap(shipsPlayer, shipsComputer));
 
         }
 
-        public static string MakeMap(Dictionary<int, string> ships)
+        public static string MakeMap(Dictionary<int, string> shipsPlayer, Dictionary<int, string> shipsComputer)
         {
-            string map = "  y\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"9 | {ships[19]} | {ships[29]} | {ships[39]} | {ships[49]} | {ships[59]} | {ships[69]} | {ships[79]} | {ships[89]} | {ships[99]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"8 | {ships[18]} | {ships[28]} | {ships[38]} | {ships[48]} | {ships[58]} | {ships[68]} | {ships[78]} | {ships[88]} | {ships[98]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"7 | {ships[17]} | {ships[27]} | {ships[37]} | {ships[47]} | {ships[57]} | {ships[67]} | {ships[77]} | {ships[87]} | {ships[97]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"6 | {ships[16]} | {ships[26]} | {ships[36]} | {ships[46]} | {ships[56]} | {ships[66]} | {ships[76]} | {ships[86]} | {ships[96]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"5 | {ships[15]} | {ships[25]} | {ships[35]} | {ships[45]} | {ships[55]} | {ships[65]} | {ships[75]} | {ships[85]} | {ships[95]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"4 | {ships[14]} | {ships[24]} | {ships[34]} | {ships[44]} | {ships[54]} | {ships[64]} | {ships[74]} | {ships[84]} | {ships[94]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"3 | {ships[13]} | {ships[23]} | {ships[33]} | {ships[43]} | {ships[53]} | {ships[63]} | {ships[73]} | {ships[83]} | {ships[93]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"2 | {ships[12]} | {ships[22]} | {ships[32]} | {ships[42]} | {ships[52]} | {ships[62]} | {ships[72]} | {ships[82]} | {ships[92]} |\n" +
-                         "   --- --- --- --- --- --- --- --- ---\n" +
-                         $"1 | {ships[11]} | {ships[21]} | {ships[31]} | {ships[41]} | {ships[51]} | {ships[61]} | {ships[71]} | {ships[81]} | {ships[91]} |\n" +
-                         "   --- --- --- --- --- --- --- --- --- x\n" +
-                         "    1   2   3   4   5   6   7   8   9";
+            string v = "   --- --- --- --- --- --- --- --- ---\t\t   --- --- --- --- --- --- --- --- ---\n";
+            string map = " --- --- --- Ваша карта --- --- --- ---\t\t--- --- --- Карта противника --- --- ---\n" +
+                         "  y\t\t\t\t\t\t  y\n" +
+                         $"{v}" +
+                         $"9 | {shipsPlayer[19]} | {shipsPlayer[29]} | {shipsPlayer[39]} | {shipsPlayer[49]} | {shipsPlayer[59]} | {shipsPlayer[69]} | {shipsPlayer[79]} | {shipsPlayer[89]} | {shipsPlayer[99]} |\t\t9 | {shipsComputer[19]} | {shipsComputer[29]} | {shipsComputer[39]} | {shipsComputer[49]} | {shipsComputer[59]} | {shipsComputer[69]} | {shipsComputer[79]} | {shipsComputer[89]} | {shipsComputer[99]} |\n" +
+                         $"{v}" +
+                         $"8 | {shipsPlayer[18]} | {shipsPlayer[28]} | {shipsPlayer[38]} | {shipsPlayer[48]} | {shipsPlayer[58]} | {shipsPlayer[68]} | {shipsPlayer[78]} | {shipsPlayer[88]} | {shipsPlayer[98]} |\t\t8 | {shipsComputer[18]} | {shipsComputer[28]} | {shipsComputer[38]} | {shipsComputer[48]} | {shipsComputer[58]} | {shipsComputer[68]} | {shipsComputer[78]} | {shipsComputer[88]} | {shipsComputer[98]} |\n" +
+                         $"{v}" +
+                         $"7 | {shipsPlayer[17]} | {shipsPlayer[27]} | {shipsPlayer[37]} | {shipsPlayer[47]} | {shipsPlayer[57]} | {shipsPlayer[67]} | {shipsPlayer[77]} | {shipsPlayer[87]} | {shipsPlayer[97]} |\t\t7 | {shipsComputer[17]} | {shipsComputer[27]} | {shipsComputer[37]} | {shipsComputer[47]} | {shipsComputer[57]} | {shipsComputer[67]} | {shipsComputer[77]} | {shipsComputer[87]} | {shipsComputer[97]} |\n" +
+                         $"{v}" +
+                         $"6 | {shipsPlayer[16]} | {shipsPlayer[26]} | {shipsPlayer[36]} | {shipsPlayer[46]} | {shipsPlayer[56]} | {shipsPlayer[66]} | {shipsPlayer[76]} | {shipsPlayer[86]} | {shipsPlayer[96]} |\t\t6 | {shipsComputer[16]} | {shipsComputer[26]} | {shipsComputer[36]} | {shipsComputer[46]} | {shipsComputer[56]} | {shipsComputer[66]} | {shipsComputer[76]} | {shipsComputer[86]} | {shipsComputer[96]} |\n" +
+                         $"{v}" +
+                         $"5 | {shipsPlayer[15]} | {shipsPlayer[25]} | {shipsPlayer[35]} | {shipsPlayer[45]} | {shipsPlayer[55]} | {shipsPlayer[65]} | {shipsPlayer[75]} | {shipsPlayer[85]} | {shipsPlayer[95]} |\t\t5 | {shipsComputer[15]} | {shipsComputer[25]} | {shipsComputer[35]} | {shipsComputer[45]} | {shipsComputer[55]} | {shipsComputer[65]} | {shipsComputer[75]} | {shipsComputer[85]} | {shipsComputer[95]} |\n" +
+                         $"{v}" +
+                         $"4 | {shipsPlayer[14]} | {shipsPlayer[24]} | {shipsPlayer[34]} | {shipsPlayer[44]} | {shipsPlayer[54]} | {shipsPlayer[64]} | {shipsPlayer[74]} | {shipsPlayer[84]} | {shipsPlayer[94]} |\t\t4 | {shipsComputer[14]} | {shipsComputer[24]} | {shipsComputer[34]} | {shipsComputer[44]} | {shipsComputer[54]} | {shipsComputer[64]} | {shipsComputer[74]} | {shipsComputer[84]} | {shipsComputer[94]} |\n" +
+                         $"{v}" +
+                         $"3 | {shipsPlayer[13]} | {shipsPlayer[23]} | {shipsPlayer[33]} | {shipsPlayer[43]} | {shipsPlayer[53]} | {shipsPlayer[63]} | {shipsPlayer[73]} | {shipsPlayer[83]} | {shipsPlayer[93]} |\t\t3 | {shipsComputer[13]} | {shipsComputer[23]} | {shipsComputer[33]} | {shipsComputer[43]} | {shipsComputer[53]} | {shipsComputer[63]} | {shipsComputer[73]} | {shipsComputer[83]} | {shipsComputer[93]} |\n" +
+                         $"{v}" +
+                         $"2 | {shipsPlayer[12]} | {shipsPlayer[22]} | {shipsPlayer[32]} | {shipsPlayer[42]} | {shipsPlayer[52]} | {shipsPlayer[62]} | {shipsPlayer[72]} | {shipsPlayer[82]} | {shipsPlayer[92]} |\t\t2 | {shipsComputer[12]} | {shipsComputer[22]} | {shipsComputer[32]} | {shipsComputer[42]} | {shipsComputer[52]} | {shipsComputer[62]} | {shipsComputer[72]} | {shipsComputer[82]} | {shipsComputer[92]} |\n" +
+                         $"{v}" +
+                         $"1 | {shipsPlayer[11]} | {shipsPlayer[21]} | {shipsPlayer[31]} | {shipsPlayer[41]} | {shipsPlayer[51]} | {shipsPlayer[61]} | {shipsPlayer[71]} | {shipsPlayer[81]} | {shipsPlayer[91]} |\t\t1 | {shipsComputer[11]} | {shipsComputer[21]} | {shipsComputer[31]} | {shipsComputer[41]} | {shipsComputer[51]} | {shipsComputer[61]} | {shipsComputer[71]} | {shipsComputer[81]} | {shipsComputer[91]} |\n" +
+                         "   --- --- --- --- --- --- --- --- --- x\t   --- --- --- --- --- --- --- --- --- x\n" +
+                         "    1   2   3   4   5   6   7   8   9\t\t    1   2   3   4   5   6   7   8   9";
             return map;
         }
 
@@ -67,9 +71,9 @@ namespace battle_ship
         {
             int key = int.Parse($"{x}{y}");
             if (int.TryParse(dict[key], out _))
-                ships[key] = "x";
+                shipsComputer[key] = "x";
             else
-                ships[key] = "o";
+                shipsComputer[key] = "o";
             return dict;
         }
 
